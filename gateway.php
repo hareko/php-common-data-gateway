05375cd0107e25b88884102e9febdeae75bb498f
Vallo Reima <vallo@vregistry.com>
2015-06-22 20:24:46 +0300

    Update gateway.php

--- gateway.php
+++ gateway.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * gateway to common methods & properties
+ * gateway to common methods and properties
  * requirements:  PHP 5.3+
  *                common class including:
- *                  common properties structure
- *                  GetProperty/SetProperty methods
- *                  customer methods & properties
+ *                  common properties setup
+ *                  _get/_set methods
+ *                  custom methods/properties
  *
  * @package     System
  * @author      Vallo Reima
@@ -19,33 +19,42 @@ class ¤ {
   private static $sep;                  /* path separator */
   public static $_;                     /* workarea */
 
-  public static function _Init($class = 'Common', $sep = '.') {
-    self::$cmi = new $class(self::$_);  /* instantiate the common class */
+  /**
+   * link to common data
+   * @param string $cln common class name
+   * @param string $sep path separator
+   */
+  public static function _Init($cln = 'Common', $sep = '.') {
+    self::$cmi = new $cln(self::$_);    /* instantiate the common class */
     self::$sep = $sep;                  /* save property path separator */
   }
 
-  public static function __callStatic($func, $args) { /* access a method */
+  /**
+   * access a method
+   * @param object $func method name
+   * @param array $args arguments list
+   * @return mixed
+   */
+  public static function __callStatic($func, $args) {
     return call_user_func_array(array(self::$cmi, $func), $args);
   }
 
-  public static function _()
-  /*
+  /**
    * get/set a property value 
-   * in:  arguments -- 1st - path string
-   *                   2nd - value (set only)
-   * out: get/set value
-   */ {
+   * @param arguments -- 1st - path string
+   *                     2nd - value (set only)
+   * @return mixed get/set value
+   */
+  public static function _() {
     $path = trim(func_get_arg(0), self::$sep);  /* remove leading/trailing separators */
     $pth = explode(self::$sep, $path);          /* split the path */
-    if (func_num_args() === 2) {
+    if (func_num_args() > 1) {
       $value = func_get_arg(1);                 /* value to set */
-      self::$cmi->SetProperty($pth, $value);    /* set a value */
+      self::$cmi->_set($pth, $value);    /* set a value */
     } else {
-      $value = self::$cmi->GetProperty($pth);   /* get a value */
+      $value = self::$cmi->_get($pth);   /* get a value */
     }
     return $value;
   }
 
 }
-
-?>
